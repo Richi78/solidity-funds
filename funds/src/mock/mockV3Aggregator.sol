097a587f5a5261eb236f3dd4bf699580da1c06e5
richi78 <ricardo.rojas.carvajal@gmail.com>
2026-07-26 22:21:32 -0400

    Add MockV3Aggregator contract for simulating price feed data

---
+++ funds/src/mock/mockV3Aggregator.sol
@@ -0,0 +1,39 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.18;
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+contract MockV3Aggregator is AggregatorV3Interface {
+    function version() external pure returns (uint256) {
+        return 4;
+    }
+
+    function decimals() external pure override returns (uint8) {
+        return 8;
+    }
+
+    function latestRoundData()
+        external
+        view
+        override
+        returns (uint80, int256, uint256, uint256, uint80)
+    {
+        return (1, 3000e8, block.timestamp, block.timestamp, 1);
+    }
+
+    function description() external pure override returns (string memory) {
+        return "Mock ETH/USD";
+    }
+
+    function getRoundData(
+        uint80 /* _roundId */
+    )
+        external
+        view
+        override
+        returns (uint80, int256, uint256, uint256, uint80)
+    {
+        return (1, 3000e8, block.timestamp, block.timestamp, 1);
+    }
+}
