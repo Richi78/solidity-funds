6bfb287b7b0959affd9eca496581f00f7408d2f6
richi78 <ricardo.rojas.carvajal@gmail.com>
2026-08-01 12:39:25 -0400

    refactor PriceConverter library to improve code structure and add getVersion function

--- funds/src/PriceConverter.sol
+++ funds/src/PriceConverter.sol
@@ -5,9 +5,12 @@ pragma solidity 0.8.18;
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 library PriceConverter{
+
+  function getVersion(AggregatorV3Interface _priceFeed) public view returns (uint256){
+    return _priceFeed.version();
+  }
+
   function getPrice(AggregatorV3Interface _priceFeed) public view returns (uint256 ){
-    // address of the contract 0x694AA1769357215DE4FAC081bf1f309aDC325306 chainlink price feed
-    // ABI
     (, int256 price, , , ) = _priceFeed.latestRoundData();
     return uint256(price * 1e10); 
   }
