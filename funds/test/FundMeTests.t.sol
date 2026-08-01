cb84f169bd01b28dd6a53d704041680e9a06725a
richi78 <ricardo.rojas.carvajal@gmail.com>
2026-08-01 12:39:34 -0400

    refactor FundMeTest to enhance test coverage and improve owner refund functionality

--- funds/test/FundMeTests.t.sol
+++ funds/test/FundMeTests.t.sol
@@ -11,16 +11,22 @@ import {PriceConverter} from "src/PriceConverter.sol";
 
 contract FundMeTest is Test {
     using PriceConverter for AggregatorV3Interface;
+    address owner = makeAddr("owner");
+    address user = makeAddr("user");
     FundMe fundMe;
     MockV3Aggregator mock;
 
     function setUp() public {
       mock = new MockV3Aggregator();
+
+      vm.startPrank(owner);
       fundMe = new FundMe(address(mock));
+      vm.stopPrank();
+
     }
 
     function testGetVersion() public {
-      uint version = fundMe.getVersion();
+      uint version = fundMe.priceFeed().getVersion();
       assertEq(version, 4);
     }
 
@@ -33,6 +39,7 @@ contract FundMeTest is Test {
       vm.expectRevert();
       fundMe.fund();
     }
+
     function testFundSuccess() public {
       address alice = makeAddr("alice");
       vm.deal(alice, 2 ether);
@@ -41,5 +48,40 @@ contract FundMeTest is Test {
       fundMe.fund{value: 1 ether}();
       assertEq(address(fundMe).balance, current + 1 ether);
       assertEq(alice.balance, 1 ether);
+    }
+
+    function testRefundAllOwner() public {
+      vm.prank(owner);
+      fundMe.refundAll();
+      assertEq(address(fundMe).balance, 0);
+      assertEq(fundMe.getFundersLength(), 0);
+    }
+
+    function testRefundAllNotOwner() public {
+      vm.expectRevert(FundMe.NotOwner.selector);
+      vm.prank(user);
+      fundMe.refundAll();
+    }
+
+    function testReceiveCallsFund() public {
+      address alice = makeAddr("alice");
+      vm.deal(alice, 5 ether);
+      vm.prank(alice);
+      (bool success, ) = address(fundMe).call{value: 0.1 ether}("");
+      assertTrue(success);
+      assertEq(fundMe.addressToAmountFunded(alice), 0.1 ether);
+      assertEq(fundMe.userAddress(0), alice);
+      assertEq(address(fundMe).balance, 0.1 ether);
+    }
+
+    function testFallbackCallsFund() public {
+      address alice = makeAddr("alice");
+      vm.deal(alice, 5 ether);
+      vm.prank(alice);
+      (bool success, ) = address(fundMe).call{value: 0.1 ether}(abi.encodeWithSignature("functionDoesNotExist()"));
+      assertTrue(success);
+      assertEq(fundMe.addressToAmountFunded(alice), 0.1 ether);
+      assertEq(fundMe.userAddress(0), alice);
+      assertEq(address(fundMe).balance, 0.1 ether);
     }
 }
